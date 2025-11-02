c9cd7fc6c0af2a1aef38e99f554203713301d38b
Hsu Sandar Swe <susandarswe.mm@gmail.com>
2025-11-02 20:32:56 +0800

    Chore: Setup Project

---
+++ ExpenseTracker.API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseTracker.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ ExpenseTracker.API/Entities/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTracker.API.Entities
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public string Category { get; set; } = string.Empty;
+    }
+}
